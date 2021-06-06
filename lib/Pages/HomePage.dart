727c96633db55b6cf2c133e4c975bf629bd4221f
mohan17100 <mohan17100@iiitnr.edu.in>
2021-06-06 16:28:13 +0530

    working on textfield,sizedbox,ElevatedButtons

---
+++ lib/Pages/HomePage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Container(
+            child: Text("hey users !"),
+          ),
+        ));
+  }
+}
